a8bca2183abe37f46a43e9b12a6fe90dff3aa650
Eladio Feijoo <eladio.feijoo@gmail.com>
2026-02-23 19:47:04 +0100

    fix(engine): restore modifier support in InterfaceRule to handle abstract symbol

--- packages/engine/src/parser/rules/interface.rule.ts
+++ packages/engine/src/parser/rules/interface.rule.ts
@@ -2,6 +2,7 @@ import { TokenType } from '../../syntax/token.types'
 import { ASTNodeType } from '../../syntax/nodes'
 import type { IParserHub } from '../core/parser.hub'
 import type { Orchestrator } from '../rule.types'
+import { ModifierRule } from './modifier.rule'
 import { BaseEntityRule } from './base-entity.rule'
 import type { StatementNode, Modifiers } from '../../syntax/nodes'
 
@@ -10,23 +11,21 @@ import type { StatementNode, Modifiers } from '../../syntax/nodes'
  */
 export class InterfaceRule extends BaseEntityRule {
   public canHandle(context: IParserHub): boolean {
-    return context.peek().type === TokenType.KW_INTERFACE
+    const skip = ModifierRule.countModifiers(context)
+    return context.lookahead(skip).type === TokenType.KW_INTERFACE
   }
 
   public parse(context: IParserHub, orchestrator: Orchestrator): StatementNode[] {
+    const pos = context.getPosition()
+    let modifiers: Modifiers = ModifierRule.parse(context)
+
     if (!context.match(TokenType.KW_INTERFACE)) {
+      context.rollback(pos)
       return []
     }
     const keywordToken = context.prev()
 
-    const modifiers: Modifiers = {
-      isAbstract: false,
-      isStatic: false,
-      isActive: false,
-      isLeaf: false,
-      isFinal: false,
-      isRoot: false,
-    }
+    modifiers = ModifierRule.parse(context, modifiers)
 
     return this.completeEntityParsing(
       context,
